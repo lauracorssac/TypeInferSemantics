29ee6a5579b07778cda9c0b397fb8ad080ed392a
Laura Corssac <lauracorssac@gmail.com>
2019-11-03 13:50:02 -0300

    Create types.py

---
+++ types.py
@@ -0,0 +1,10 @@
+# ideia de centralizar todas os tipos aqui. 
+# Para no retorno das funções ao invés de por exemplo "INT"
+# colocar "TYPE.INT"
+
+class TYPE():
+    INT = "INT"
+    BOOL = "BOOL"
+    
+    def FUNC(type_param, type_body):
+        return "(" + type_param + ")" + " -> " + type_body
