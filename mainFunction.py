125db010895495d6be3a1b64ad05f4c3bda09881
Laura Corssac <lauracorssac@gmail.com>
2019-11-03 19:34:46 -0300

    main func

---
+++ mainFunction.py
@@ -0,0 +1,24 @@
+#functions
+
+import TInt
+import TBool
+import Top
+import Top2
+import Tif
+import Tvar
+import TFn
+import Tapp
+import Tlet
+import TLetrec
+import TRaise
+import Ttry
+import Thd
+import Ttl
+import TIsempty
+
+#definitions
+import Type 
+
+
+def main_function(environment, term):
+  return Type.Error
