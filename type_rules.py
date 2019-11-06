54ecf074852d603dc06160063dcd5bc3998a063b
Laura Corssac <lauracorssac@gmail.com>
2019-11-05 21:28:43 -0300

    Create type_rules.py

---
+++ type_rules.py
@@ -0,0 +1,15 @@
+import Tint
+import Tbool
+import Top
+import Top2
+import Tif
+import Tvar
+import Tfn
+import Tapp
+import Tlet
+import Tletrec
+import Traise
+import Ttry
+import Thd
+import Ttl
+import Tisempty
