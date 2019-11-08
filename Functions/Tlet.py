52059b11b130606cdbddc819a3c83b23ad95a87c
lauracorssac <lauracorssac@gmail.com>
2019-11-07 22:35:08 -0300

    t let and t var

--- Functions/Tlet.py
+++ Functions/Tlet.py
@@ -1,17 +1,100 @@
-import mainFunction
+#import mainFunction
 from Definitions.types import TYPE
 
-# o que eu to pesando como entrada mas por enquanto mockei: [let-x-T, e1, e2]
+# Nodo esperado
+# {
+#   description: "qqr coisa",
+#   elements: { "x" : nome_var, "T", "TYPE.INT", "e1": nodo_1, "e2" : nodo_2 }
+# }
 
-def t_let(environment, term):
-    
-    param = "x" # como??
-    param_type = "INT" #como??
-    e1 = term[1]
-    e2 = term[2]
-    
-    if main_function(environment, e1) == param_type:
-        environment[param] = param_type
-        return main_function(environment, e2)
-    
+def main_function(environment, node):
+    return environment[node["description"]]
+
+def t_let(environment, node):
+
+    if environment and node and "elements" in node:
+        elements = node["elements"]
+
+        if "x" in elements and "T" in elements and "e1" in elements and "e2" in elements:
+            param = elements["x"]
+            param_type = elements["T"]
+            e1 = elements["e1"]
+            e2 = elements["e2"]
+
+            if main_function(environment, e1) == param_type:
+                environment[param] = param_type
+                return main_function(environment, e2)
+
     return TYPE.ERROR
+
+node_e2 = {
+    "description": "y",
+}
+
+node_e1 = {
+    "description": "z",
+}
+
+node_let = {
+    "description": "let",
+    "elements": {
+        "x" : "y",
+        "T": TYPE.INT,
+        "e1": node_e1,
+        "e2" : node_e2
+    }
+}
+
+# entrada let y:INT = "z" in y
+
+# teste sucesso
+assert(t_let({"z": TYPE.INT}, node_let) == TYPE.INT)
+# entrada tipo invalido
+assert(t_let({"z": TYPE.BOOL}, node_let) == TYPE.ERROR)
+# entrada nao declarada
+assert(t_let({}, node_let) == TYPE.ERROR)
+assert(t_let(None, node_let) == TYPE.ERROR)
+assert(t_let({"z": TYPE.BOOL}, None) == TYPE.ERROR)
+
+node_e22 = {
+    "description": "w",
+}
+
+node_e12 = {
+    "description": "z",
+}
+
+node_let2 = {
+    "description": "let",
+    "elements": {
+        "x" : "y",
+        "T": TYPE.INT,
+        "e1": node_e12,
+        "e2" : node_e22
+    }
+}
+
+# entrada let y:INT = "z" in "w"
+
+# w é um bool, entrada certa
+assert(t_let({"w": TYPE.BOOL, "z": TYPE.INT}, node_let2) == TYPE.BOOL)
+
+# w é um bool, entrada errada
+assert(t_let({"w": TYPE.BOOL, "z": TYPE.BOOL}, node_let2) == TYPE.ERROR)
+
+node_let3 = {
+    "description": "let",
+    "elements": {
+        "x" : "w",
+        "T": TYPE.BOOL,
+        "e1": node_e12,
+        "e2" : node_e22
+    }
+}
+
+# entrada let w:BOOL = "z" in "w"
+
+# entrada certa, muda dict
+assert(t_let({"w": TYPE.INT, "z": TYPE.BOOL}, node_let3) == TYPE.BOOL)
+
+print("Testes t-let passaram")
--- Functions/Tvar.py
+++ Functions/Tvar.py
@@ -1,17 +1,32 @@
 import mainFunction
 from Definitions.types import TYPE
 
-def t_var(environment, term):
-    
-    if environment and term in environment:
-        return environment[term]
+def t_var(environment, node):
+
+    if node and "description" in node:
+        term = node["description"]
+        if environment and term in environment:
+            return environment[term]
+        else:
+            return TYPE.ERROR
     else:
-        return TYPE.ERROR 
-        
-assert(t_var({"x": TYPE.BOOL}, "x") == TYPE.BOOL)
-assert(t_var({"x": TYPE.BOOL}, "y") == TYPE.ERROR)
-assert(t_var( {"z": TYPE.FUNC(TYPE.INT, TYPE.INT), "x": TYPE.BOOL}, "y") == TYPE.ERROR)
-assert(t_var( {"z": TYPE.FUNC(TYPE.INT, TYPE.INT), "x": TYPE.BOOL}, "x") == TYPE.BOOL)
-assert(t_var( {"z": TYPE.FUNC(TYPE.INT, TYPE.INT), "x": TYPE.BOOL}, "z") == TYPE.FUNC(TYPE.INT, TYPE.INT))
-assert(t_var({}, "x") == TYPE.ERROR)
-assert(t_var(None, "x") == TYPE.ERROR)
+        return TYPE.ERROR
+
+
+node_x = { "description": "x"}
+node_y = { "description": "y"}
+node_z = { "description": "z"}
+node_fake = {"no_desc": "y"}
+
+assert(t_var({"x": TYPE.BOOL}, node_x) == TYPE.BOOL)
+assert(t_var({"x": TYPE.BOOL}, node_y) == TYPE.ERROR)
+
+assert(t_var( {"z": TYPE.FUNC(TYPE.INT, TYPE.INT), "x": TYPE.BOOL}, node_y) == TYPE.ERROR)
+assert(t_var( {"z": TYPE.FUNC(TYPE.INT, TYPE.INT), "x": TYPE.BOOL}, node_x) == TYPE.BOOL)
+assert(t_var( {"z": TYPE.FUNC(TYPE.INT, TYPE.INT), "x": TYPE.BOOL}, node_z) == TYPE.FUNC(TYPE.INT, TYPE.INT))
+assert(t_var({}, node_x) == TYPE.ERROR)
+assert(t_var(None, node_x) == TYPE.ERROR)
+assert(t_var({"x": TYPE.BOOL}, node_fake) == TYPE.ERROR)
+assert(t_var({"x": TYPE.BOOL}, None) == TYPE.ERROR)
+
+print("testes t-var passaram")
