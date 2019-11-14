e6437f0da0bb6a973056163eedd9ed52e9a66031
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2019-11-13 22:21:15 -0300

    refactor: use pytest template

--- Tests/test_tfun.py
+++ Tests/test_tfun.py
@@ -5,20 +5,19 @@ from Functions.batata import *
 from Functions.Tfun import *
 from Definitions.types import TYPE
 
-# class TestTfun:
-    # def test_tfun_ok():
-node = {
-    "description": "tfun",
-    "elements": {
-        "e1": "x",
-        "e2": TYPE.INT,
-        "e3": {
-            "description": "tvar",
-            "elements": {
-                "e1": "x"
+def test_tfun_ok():
+    node = {
+        "description": "tfun",
+        "elements": {
+            "e1": "x",
+            "e2": TYPE.INT,
+            "e3": {
+                "description": "tvar",
+                "elements": {
+                    "e1": "x"
+                }
             }
         }
     }
-}
-environment = {}
-assert t_fun(environment, node) == "(" + TYPE.INT + ") -> " + TYPE.INT
+    environment = {}
+    assert t_fun(environment, node) == "(" + TYPE.INT + ") -> " + TYPE.INT
