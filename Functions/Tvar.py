b903d6093d98bc02ff100a55a1efa6207a7001ac
Andy Ruiz Garramones <andyfutbolclip@yahoo.es>
2019-11-13 21:43:04 -0300

    fix: tvar sanity check

--- Functions/Tvar.py
+++ Functions/Tvar.py
@@ -1,13 +1,24 @@
 import main
 from Definitions.types import TYPE
 
+# Input example:
+#
+# node = {
+#   "description": "tvar"
+#   "elements": {"e1": "x"}
+# }
+#
+# environment = {
+#   "x": TYPE.INT
+# }
+#
+# Return:
+#   TYPE.INT
 def t_var(environment, node):
+    from main import infer_type
 
-    if node and "description" in node:
-        term = node["description"]
-        if environment and term in environment:
-            return environment[term]
-        else:
-            return TYPE.ERROR
+    e1 = node['elements']['e1']
+    if environment and e1 in environment:
+        return environment[e1]
     else:
         return TYPE.ERROR
