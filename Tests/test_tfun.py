633d0ea65c75fab7233222fc5f9b3ca278944cd7
Laura <lauracorssac@gmail.com>
2019-11-15 14:05:20 -0300

    made a class for t_fun tests

--- Tests/test_tfun.py
+++ Tests/test_tfun.py
@@ -1,23 +1,25 @@
 import sys
 sys.path.insert(1, '../')
 
-from Functions.batata import *
 from Functions.Tfun import *
 from Definitions.types import TYPE
 
-def test_tfun_ok():
-    node = {
-        "description": "tfun",
-        "elements": {
-            "e1": "x",
-            "e2": TYPE.INT,
-            "e3": {
-                "description": "tvar",
-                "elements": {
-                    "e1": "x"
+class TestTFun:
+
+    def test_tfun_ok(self):
+        node = {
+            "description": "tfun",
+            "elements": {
+                "e1": "x",
+                "e2": TYPE.INT,
+                "e3": {
+                    "description": "tvar",
+                    "elements": {
+                        "e1": "x"
+                    }
                 }
             }
         }
-    }
-    environment = {}
-    assert t_fun(environment, node) == "(" + TYPE.INT + ") -> " + TYPE.INT
+        environment = {}
+        assert t_fun(environment, node) == "(" + TYPE.INT + ") -> " + TYPE.INT
+        assert t_fun(environment, node) == TYPE.FUNC(TYPE.INT, TYPE.INT)
