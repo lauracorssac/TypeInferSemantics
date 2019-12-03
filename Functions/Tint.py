55911bdacbc1d52c6fe782f763b55e236fffbcf9
Marlize Ramos <lize_rb@hotmail.com>
2019-12-03 19:49:13 -0300

    Add Tint

--- Functions/Tint.py
+++ Functions/Tint.py
@@ -1,4 +1,36 @@
 from Definitions.types import TYPE
 
-def t_int(environment, term):
-    return TYPE.ERROR
+# Input example:
+#
+# node = {
+#   "description": "tint"
+#   "elements": {
+#        "e1": "7"
+#   }
+# }
+#
+# environment = {}
+#
+# Return:
+#    TYPE.INT
+#
+
+def validParameters(environment, node):
+    if node and "description" in node and "elements" in node and "e1" in node["elements"]:
+        return True
+    else:
+        return False
+
+def t_int(environment, node):
+    if not validParameters(environment, node):
+        return TYPE.ERROR
+
+    from main import infer_type
+
+    value = int(node['elements']['e1'])
+
+    if type(value) == int:
+        return TYPE.INT
+    else:
+        return TYPE.ERROR
+
