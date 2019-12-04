548eb59f3b6032c674bf92212bec53e0f003ebb1
Marlize Ramos <lize_rb@hotmail.com>
2019-12-03 21:31:31 -0300

    Add Tbool

--- Functions/Tbool.py
+++ Functions/Tbool.py
@@ -1,4 +1,31 @@
 from Definitions.types import TYPE
 
+# Input example:
+#
+# node = {
+#   "description": "tbool"
+#   "elements": {
+#        "e1": "true"
+#   }
+# }
+#
+# environment = {}
+#
+# Return:
+#    TYPE.BOOL
+#
+
+def validParameters(environment, node):
+    if node and "description" in node and "elements" in node and "e1" in node["elements"]:
+        return True
+    else:
+        return False
+
 def t_bool(environment, term):
-    return TYPE.ERROR
+
+    word = node['elements']['e1'].lower()
+
+    if (word == "true") or (word == "false")
+        return TYPE.BOOL
+    else:
+        return TYPE.ERROR
---
+++ Tests/test_tbool.py
@@ -0,0 +1,58 @@
+import sys
+sys.path.insert(1, '../')
+
+from Functions.Tint import *
+from Definitions.types import TYPE
+
+class TestTbool:
+
+    def test_tbool_lower_true(self):
+        node = {
+            "description": "tbool",
+            "elements": {
+                "e1": "true"
+            }
+        }
+
+    def test_tbool_lower_false(self):
+        node = {
+            "description": "tbool",
+            "elements": {
+                "e1": "false"
+            }
+        }
+
+    def test_tbool_upper_true(self):
+            node = {
+                "description": "tbool",
+                "elements": {
+                    "e1": "TRUE"
+                }
+            }
+
+    def test_tbool_upper_false(self):
+        node = {
+            "description": "tbool",
+            "elements": {
+                "e1": "FALSE"
+            }
+        }
+
+    def test_tbool_mid_false(self):
+        node = {
+            "description": "tbool",
+            "elements": {
+                "e1": "False"
+            }
+        }    
+
+    def test_tbool_error(self):
+        node = {
+            "description": "tbool",
+            "elements": {
+                "e1": "8"
+            }
+        }    
+    
+        environment = {}
+        assert t_bool(environment, node) == TYPE.BOOL 
