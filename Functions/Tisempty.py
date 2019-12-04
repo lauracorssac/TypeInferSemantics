14f06b92b4b27fae34485a5d0f557b6242341108
GuilhermeGS <ggsartori@inf.ufrgs.br>
2019-12-04 15:50:33 -0300

    #9 DOC: Estrutura de dados Tisempty e Tlist definidas, contrato de t_isempty comentado!

--- Functions/Tisempty.py
+++ Functions/Tisempty.py
@@ -1,4 +1,41 @@
 from Definitions.types import TYPE
 
+# Input example:
+#
+# isempty [1, 2]
+#
+# node = {
+#   "description": "tisempty"
+#   "elements": {
+#        "e1": {
+#            "description": "tlist",
+#            "elements": {
+#                 "e1" : {
+#                       "description": "tint",
+#                       "elements": {"e1": "1"}
+#                 },
+#                 "e2": {
+#                      "description": "tlist",
+#                      "elements": {
+#                           "e1": {
+#                                "description": "tint",
+#                                "elements": {"e1": "2"}
+#                           },
+#                           "e2": {
+#                                "description": "tempty"
+#                                "elements": {"e1": "empty"}
+#                           }
+#                      }
+#                 }
+#             }
+#         }
+#   }
+#
+# environment = {}
+#
+# Return:
+#   TYPE.BOOL
+#
+
 def t_isempty(environment, term):
     return TYPE.ERROR
