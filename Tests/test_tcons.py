d6f39cd8c271c87c30155a11e20b469311d47f69
GuilhermeGS <ggsartori@inf.ufrgs.br>
2019-12-08 16:06:33 -0300

    One more test for tcons

--- Tests/test_tcons.py
+++ Tests/test_tcons.py
@@ -226,6 +226,40 @@ class TestTcons:
         environment = {}
         assert t_cons(environment, node) == TYPE.LIST(TYPE.LIST(TYPE.INT))
 
+    def test_tcons_nested_raiseint2_ok(self):
+        node = {
+            "description": "tcons",
+            "elements": {
+                "e1": {
+                    "description": "tcons",
+                    "elements": {
+                        "e1" : {
+                            "description": "tint",
+                            "elements": {"e1": "1"}
+                        },
+                        "e2" : {
+                            "description": "tcons",
+                            "elements": {
+                                "e1": {
+                                    "description": "traise",
+                                    "elements": {"e1": "raise"}
+                                },
+                                "e2": {
+                                    "description": "tempty",
+                                    "elements": {"e1": "empty"}
+                                }
+                            }
+                        }
+                    }
+                },
+                "e2": {
+                    "description": "tempty",
+                    "elements": {"e1" : "empty"}
+                }
+            }
+        }
+        environment = {}
+        assert t_cons(environment, node) == TYPE.LIST(TYPE.LIST(TYPE.INT))
 
     def test_tcons_int_ok(self):
         node = {
